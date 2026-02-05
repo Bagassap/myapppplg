2b3e0a3fec5e85d0ba268ee98cf1af6afca4f467
kejum4nis <misterizeus@gmail.com>
2026-02-05 14:47:03 +0700

    fix api komentar route params error

--- app/api/informasi/komentar/route.ts
+++ app/api/informasi/komentar/route.ts
@@ -1,25 +1,21 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
-export async function POST(
-    request: Request,
-    { params }: { params: { id: string } }
-) {
+export async function POST(request: Request) {
     try {
-        const informasiId = parseInt(params.id);
         const body = await request.json();
-        const { nama, isi, tanggal } = body;
+        const { nama, isi, tanggal, informasiId } = body;
 
-        if (!isi || !nama) {
+        if (!isi || !nama || !informasiId) {
             return NextResponse.json(
-                { error: "Nama dan Isi komentar wajib diisi" },
+                { error: "Nama, Isi, dan ID Informasi wajib diisi" },
                 { status: 400 }
             );
         }
 
         const newComment = await prisma.komentar.create({
             data: {
-                informasiId,
+                informasiId: parseInt(informasiId),
                 nama,
                 isi,
                 tanggal: tanggal || new Date().toISOString().split("T")[0],
