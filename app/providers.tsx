a7958b1b215935372f83504b4510ceee71dffc85
kejum4nis <misterizeus@gmail.com>
2026-03-13 17:36:23 +0700

    login

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,15 +1,12 @@
 "use client";
 
-import { SessionProvider } from "next-auth/react";
 import { SidebarProvider } from "@/contexts/SidebarContext";
 import { ThemeProvider } from "@/contexts/ThemeContext";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <SessionProvider>
-      <ThemeProvider>
-        <SidebarProvider>{children}</SidebarProvider>
-      </ThemeProvider>
-    </SessionProvider>
+    <ThemeProvider>
+      <SidebarProvider>{children}</SidebarProvider>
+    </ThemeProvider>
   );
 }
